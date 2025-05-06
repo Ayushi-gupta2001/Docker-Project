824dec1acb100fced2737598b57ae469e5a9b421
Ayushi-gupta2001 <g.dibs2001@gmail.com>
2025-05-06 17:46:09 +0530

    fixing aws issue

--- infra/main.tf
+++ infra/main.tf
@@ -16,56 +16,56 @@ module "web_ecr_image" {
 } 
 
 /* Module for ECS Service creation */
-module "web_ecs_service" {
-  source      = "./module/ECS_SERVICE"
-  ecs_cluster = "web_ecs_cluster"
-  ecs_service = "web_ecs_service"
-  iam_role    = module.web_iam_role.web_iam_role_ecs
-  containers = {
-    "client_container" = {
-      task_defination = "client_task_defination"
-      container_name  = "client_container"
-      ecr_image       = module.web_ecr_image.ecs_images["client"].image_uri
-      host_port       = 3001
-      container_port  = 3000
-      environment     = var.client_env
-      volume_storage  = "client_storage"
-    }
-    "server_container" = {
-      task_defination = "server_task_defination"
-      container_name  = "server_container"
-      ecr_image       = module.web_ecr_image.ecs_images["server"].image_uri
-      host_port       = 9000
-      container_port  = 9000
-      environment     = var.server_env
-      volume_storage  = "server_storage"
-    }
-    "database_container" = {
-      task_defination = "database_task_defination"
-      container_name  = "database_container"
-      ecr_image       = "postgres:latest"
-      host_port       = 7890
-      container_port  = 5432
-      environment     = var.database_env
-      volume_storage  = "database_storage"
-    }
-  }
-  subnet = module.web_vpc.subnet_id
-  security_groups = module.web_security_group.security_group
-  task_defination = {
-    "client_task_defination" = {
+# module "web_ecs_service" {
+#   source      = "./module/ECS_SERVICE"
+#   ecs_cluster = "web_ecs_cluster"
+#   ecs_service = "web_ecs_service"
+#   iam_role    = module.web_iam_role.web_iam_role_ecs
+#   containers = {
+#     "client_container" = {
+#       task_defination = "client_task_defination"
+#       container_name  = "client_container"
+#       ecr_image       = module.web_ecr_image.ecs_images["client"].image_uri
+#       host_port       = 3001
+#       container_port  = 3000
+#       environment     = var.client_env
+#       volume_storage  = "client_storage"
+#     }
+#     "server_container" = {
+#       task_defination = "server_task_defination"
+#       container_name  = "server_container"
+#       ecr_image       = module.web_ecr_image.ecs_images["server"].image_uri
+#       host_port       = 9000
+#       container_port  = 9000
+#       environment     = var.server_env
+#       volume_storage  = "server_storage"
+#     }
+#     "database_container" = {
+#       task_defination = "database_task_defination"
+#       container_name  = "database_container"
+#       ecr_image       = "postgres:latest"
+#       host_port       = 7890
+#       container_port  = 5432
+#       environment     = var.database_env
+#       volume_storage  = "database_storage"
+#     }
+#   }
+#   subnet = module.web_vpc.subnet_id
+#   security_groups = module.web_security_group.security_group
+#   task_defination = {
+#     "client_task_defination" = {
 
-    },
-    "server_task_defination" = {
+#     },
+#     "server_task_defination" = {
 
-    },
-    "postgress_task_defination" = {
+#     },
+#     "postgress_task_defination" = {
 
-    }
-  }
-  lb_listener_group = module.web_load_balancer.web_lb_target_group
-  container_name = "swde"
-}
+#     }
+#   }
+#   lb_listener_group = module.web_load_balancer.web_lb_target_group
+#   container_name = "swde"
+# }
 
 /* Module for VPC */
 module "web_vpc" {
