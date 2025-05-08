330d02b6be20282eee3df65b238a06f6e6432cce
Ayushi-gupta2001 <g.dibs2001@gmail.com>
2025-05-08 18:50:40 +0530

    fixing

--- infra/main.tf
+++ infra/main.tf
@@ -74,7 +74,7 @@ module "web_security_group" {
 /* Module for load_balancer */
 module "web_load_balancer" {
     source = "./module/load-balancer"
-    load_balancer = "web_load_balancer"
+    load_balancer = "web-load-balancer"
     subnet_id = module.web_vpc.subnet_id
     vpc_id = module.web_vpc.vpc_id
     security_group = module.web_security_group.security_group
